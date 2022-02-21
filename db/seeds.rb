c67fc2f79a6fa65ecb90998b0f71d1284891f6b6
DanaGS <danags024@gmail.com>
2022-02-21 16:45:59 +0000

    add db seeds with plant's daily water need

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+PlantWaterNeed.create([ # Set default values for each plant's water need group
+  { daily_water_need_factor: "0.143" }, # Water loving
+  { daily_water_need_factor: "0.086" }, # Drought Tolerant
+  { daily_water_need_factor: "0.043" }  # Succulent
+])
