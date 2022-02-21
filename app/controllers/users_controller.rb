560ac0bb4507e80e17dd051dd53e778dd5570f11
DanaGS <danags024@gmail.com>
2022-02-21 10:07:20 +0000

    add nested attributes for location in user registration form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,15 @@
 class UsersController < ApplicationController
+
   def new
     @user = User.new #'@'veriable - instance variable
+    @user.build_location # @user.location.new
   end
+
   def create
     @user = User.new(user_params)
-    @user.location_id = 1 # for testing
+    #@user.location_id = 1 # for testing
+    @user.location = Location.where(city: @user.location.city).first_or_create
+
     if @user.save
       session[:user_id] = @user.id
       redirect_to root_path, notice: "Successfully created account"
@@ -16,6 +21,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    params.require(:user).permit(:email, :password, :password_confirmation, location_attributes: [:city])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
   belongs_to :location
   has_many :plants
-
   has_secure_password
+
+  accepts_nested_attributes_for :location
+
+  validates_associated :location
   validates :email, presence: true, format: {with: URI::MailTo::EMAIL_REGEXP, message: "Invalid email address"}, uniqueness: { case_sensitive: true }
 end
